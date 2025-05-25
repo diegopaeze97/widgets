fb864341d6a2bd3bd74e41a7901694c131777928
diegopaeze97 <diegopaeze@gmail.com>
2025-05-25 10:55:07 -0400

    modified:   image-draganddrop.js

--- image-draganddrop.js
+++ image-draganddrop.js
@@ -60,7 +60,6 @@
             box-shadow: 0 2px 4px rgba(0,0,0,0.1);
             overflow: hidden;
         }
-            749576
         .widget-image-container img {
             width: 100%;
             height: 100%;
